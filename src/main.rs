bb18a0b448f94f627844ef0888ababdf2b370453
RyanQueirozS <ryanqueiroz@proton.me>
2024-01-30 14:55:09 -0300

    Improved code readability and improved board appearence

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@ use std::time::{SystemTime, UNIX_EPOCH};
 #[derive(PartialEq, Debug, Copy, Clone)]
 struct Cell {
     number: u8,
-    is_not_clicked: bool,
+    is_clicked: bool,
 }
 
 fn generate_begginer_board() -> [[Cell; 9]; 9] {
     let mut count = 0;
     let mut board = [[Cell {
         number: 0,
-        is_not_clicked: false,
+        is_clicked: false,
     }; 9]; 9];
 
     rand::srand(
@@ -28,7 +28,7 @@ fn generate_begginer_board() -> [[Cell; 9]; 9] {
         if board[x][y].number != 9 {
             board[x][y] = Cell {
                 number: 9, // 9 will be the mine number
-                is_not_clicked: false,
+                is_clicked: false,
             };
             count += 1;
         }
@@ -56,7 +56,7 @@ fn generate_begginer_board() -> [[Cell; 9]; 9] {
                 }
                 board[i][j] = Cell {
                     number: count,
-                    is_not_clicked: false,
+                    is_clicked: false,
                 };
             }
         }
@@ -65,11 +65,95 @@ fn generate_begginer_board() -> [[Cell; 9]; 9] {
     return board;
 }
 
+async fn draw_board(board: &mut [[Cell; 9]; 9]) {
+    let cell_width = screen_width() / board.len() as f32;
+    let cell_height = screen_height() / board[0].len() as f32;
+
+    let bomb = load_texture("./src/assets/bomb.png").await.unwrap();
+    bomb.set_filter(FilterMode::Nearest);
+    let text_params = DrawTextureParams {
+        dest_size: Some(vec2(cell_width - 10.0, cell_height - 10.0)),
+        ..Default::default()
+    };
+
+    for i in 0..board.len() {
+        for j in 0..board[i].len() {
+            draw_rectangle(
+                cell_width * i as f32,
+                cell_height * j as f32,
+                cell_width - 1.0,
+                cell_height - 1.0,
+                DARKGRAY,
+            );
+
+            let number = board[j][i].number;
+            if board[i][j].is_clicked {
+                if board[j][i].number == 9 {
+                    draw_texture_ex(
+                        &bomb,
+                        cell_width * i as f32,
+                        cell_height * j as f32,
+                        WHITE,
+                        text_params.clone(),
+                    );
+                } else {
+                    let color: Color;
+                    match number {
+                        1 => color = DARKGREEN,
+                        2 => color = DARKBLUE,
+                        3 => color = VIOLET,
+                        4 => color = PINK,
+                        5 => color = MAROON,
+                        6 => color = BROWN,
+                        7 => color = YELLOW,
+                        8 => color = RED,
+                        _ => color = BLACK,
+                    }
+                    draw_rectangle(
+                        cell_width * i as f32,
+                        cell_height * j as f32,
+                        cell_width - 1.0,
+                        cell_height - 1.0,
+                        GRAY,
+                    );
+                    if number != 0 {
+                        draw_text(
+                            &number.to_string(),
+                            cell_width * i as f32,
+                            cell_height * j as f32 + cell_height,
+                            100.0,
+                            color,
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+async fn handle_input(board: &mut [[Cell; 9]; 9]) {
+    let cell_width = screen_width() / board.len() as f32;
+    let cell_height = screen_height() / board[0].len() as f32;
+
+    if is_mouse_button_pressed(MouseButton::Left) {
+        let _mouse_pos = mouse_position();
+        let mousex = (_mouse_pos.0 / cell_width) as usize;
+        let mousey = (_mouse_pos.1 / cell_height) as usize;
+        board[mousex][mousey].is_clicked = true;
+
+        if board[mousey][mousex].number == 9 {
+            for i in 0..board.len() {
+                for j in 0..board[i].len() {
+                    board[i][j].is_clicked = true;
+                }
+            }
+        }
+    }
+}
+
 #[macroquad::main("Minesweeper")]
 async fn main() {
     let mut board: [[Cell; 9]; 9] = generate_begginer_board();
-    let bomb = load_texture("./src/assets/bomb.png").await.unwrap();
-    bomb.set_filter(FilterMode::Nearest);
 
     for i in 0..board.len() {
         for j in 0..board[i].len() {
@@ -80,55 +164,12 @@ async fn main() {
 
     request_new_screen_size(1024.0, 768.0);
     let mut _mouse_pos: (f32, f32);
+
+    clear_background(BLACK);
     loop {
-        let cell_width = screen_width() / board.len() as f32;
-        let cell_height = screen_height() / board[0].len() as f32;
+        handle_input(&mut board).await;
+        draw_board(&mut board).await;
 
-        let text_params = DrawTextureParams {
-            dest_size: Some(vec2(cell_width - 10.0, cell_height - 10.0)),
-            ..Default::default()
-        };
-
-        clear_background(BLACK);
-        if is_mouse_button_pressed(MouseButton::Left) {
-            _mouse_pos = mouse_position();
-            let mousex = (_mouse_pos.0 / cell_width) as usize;
-            let mousey = (_mouse_pos.1 / cell_height) as usize;
-            board[mousex][mousey].is_not_clicked = true;
-        }
-        for i in 0..board.len() {
-            for j in 0..board[i].len() {
-                draw_rectangle(
-                    cell_width * i as f32,
-                    cell_height * j as f32,
-                    cell_width - 1.0,
-                    cell_height - 1.0,
-                    DARKGRAY,
-                );
-
-                let number = board[j][i].number;
-                if board[i][j].is_not_clicked {
-                    if board[j][i].number == 9 {
-                        draw_texture_ex(
-                            &bomb,
-                            cell_width * i as f32,
-                            cell_height * j as f32,
-                            WHITE,
-                            text_params.clone(),
-                        );
-                    } else {
-                        draw_text(
-                            &number.to_string(),
-                            cell_width * i as f32,
-                            cell_height * j as f32 + cell_height,
-                            100.0,
-                            BLACK,
-                        )
-                    }
-                } else {
-                }
-            }
-        }
         next_frame().await;
     }
 }
